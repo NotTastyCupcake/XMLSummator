171fa2a64c017ca5aa61461a5d1789e5a1eab49d
NotTastyCupcake <andrei28122000@gmail.com>
2022-06-06 16:24:37 +0500

    refactor: Изменил целевую платформу для ApplicationCore на .Net Framework 4.6.1

---
+++ tests/NUnitTests/ApplecationCore/FileWorkerServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using Metcom.XMLSummator.ApplicationCore.Services;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTests.ApplecationCore
+{
+    public class FileWorkerServiceTests
+    {
+        [TestCase()]
+        public void ShouldReturnCorrentXml()
+        {
+            var fileWorker = new FileWorkerService();
+        }
+
+        // Arrenge
+        // Act
+        // Assert 
+    }
+}
